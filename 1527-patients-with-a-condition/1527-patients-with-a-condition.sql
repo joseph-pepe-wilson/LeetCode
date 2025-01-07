ebeb79c96d6da3b68b1478fadfd42150a89e660d
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-01-07 23:08:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+select 
+    patient_id,
+    patient_name,
+    conditions
+from
+    Patients
+where
+    conditions regexp '(^| )DIAB1[^ ]*( |$'
+order by
+    patient_id;

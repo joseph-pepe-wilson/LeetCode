d1c8d0897b401584f9d3e9184555246838677365
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-02-08 15:05:46 +0530

    Time: 692 ms (13.23%), Space: 151.6 MB (18.14%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.py
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.py
@@ -1,32 +1,27 @@
-from sortedcontainers import SortedDict, SortedSet
+from sortedcontainers import SortedSet
+
+
 class NumberContainers:
+  def __init__(self):
 
-    def __init__(self):
-        self.index_to_number = {}
-        self.number_to_index = collections.defaultdict(SortedSet)
+    self.number_to_indices = collections.defaultdict(SortedSet)
+    self.index_to_number = {}
 
-    def change(self, index: int, number: int) -> None:
-        # If the index already exists in index_to_number, remove the old number from number_to_index
-        if index in self.index_to_number:
-            old_number = self.index_to_number[index]
-            self.number_to_index[old_number].remove(index)
-            if not self.number_to_index[old_number]:
-                del self.number_to_index[old_number]
+  def change(self, index: int, number: int) -> None:
+    if index in self.index_to_number:
+      original_number = self.index_to_number[index]
+      self.number_to_indices[original_number].remove(index)
 
-        self.index_to_number[index] = number # Update the index_to_number dictionary
+      if len(self.number_to_indices[original_number]) == 0:
+        del self.number_to_indices[original_number]
 
-        if number not in self.number_to_index:
-            self.number_to_index[number] = set()
-        self.number_to_index[number].add(index) # Add the index to the number_to_index dictionary
+    self.index_to_number[index] = number
+    self.number_to_indices[number].add(index)
 
-    def find(self, number: int) -> int:
-        # If the number is in the number_to_index dictionary, return the smallest index
-        if number in self.number_to_index and self.number_to_index[number]:
-            return min(self.number_to_index[number])
-        else:
-            return -1
-
-
+  def find(self, number: int) -> int:
+    if number in self.number_to_indices:
+      return self.number_to_indices[number][0]
+    return -1
 
 # Your NumberContainers object will be instantiated and called as such:
 # obj = NumberContainers()

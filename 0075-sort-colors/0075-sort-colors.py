a7c53bac4dae3815f3b2ec891b292df803c71eb6
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-05-17 20:39:05 +0530

    Time: 0 ms (100%), Space: 17.9 MB (11.82%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+       
+        # Initialize three pointers
+        left, mid, right = 0, 0, len(nums) - 1
+        
+        # Traverse through the list
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1

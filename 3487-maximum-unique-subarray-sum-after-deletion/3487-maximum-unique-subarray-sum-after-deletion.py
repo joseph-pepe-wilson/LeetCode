424448547204740f9808005e6bbd5a7ae85a2d29
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-07-25 14:35:27 +0530

    Time: 0 ms (100%), Space: 18 MB (11.24%) - LeetHub

--- 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.py
+++ 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.py
@@ -1,15 +1,9 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
-        seen = set()
-        max_sum = curr_sum = left = 0
-
-        for right in range(len(nums)):
-            while nums[right] in seen:
-                seen.remove(nums[left])
-                curr_sum -= nums[left]
-                left += 1
-            seen.add(nums[right])
-            curr_sum += nums[right]
-            max_sum = max(max_sum, curr_sum)
-
-        return max_sum
+        s = 0
+        for i in set(nums) :
+            if i > 0:
+                s += i
+        if s == 0:
+            return max(nums)
+        return s

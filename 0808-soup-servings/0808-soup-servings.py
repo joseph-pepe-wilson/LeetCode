f9bf9e89356d8e4e833c4b625b2cac845ebfcdcc
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-08-08 21:11:46 +0530

    Time: 7 ms (93.07%), Space: 19.4 MB (64.03%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n >= 4800:
+            return 1.0
+
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def dp(a, b):
+            if a <= 0 and b <= 0:
+                return 0.5
+            if a <= 0:
+                return 1.0
+            if b <= 0:
+                return 0.0
+
+            return 0.25 * (
+                dp(a - 4, b) +
+                dp(a - 3, b - 1) +
+                dp(a - 2, b - 2) +
+                dp(a - 1, b - 3)
+            )
+
+        scaled_n = (n + 24) // 25  # Round up to nearest multiple of 25
+        return dp(scaled_n, scaled_n)

72d81a5935bddd2619b60d94fcb91fd192792e9c
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-10-28 22:05:07 +0530

    Time: 47 ms (66.49%), Space: 17.7 MB (54.26%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
@@ -1,26 +1,14 @@
 class Solution:
-    def countValidSelections(self, nums):
-        def simulate(start, dir):
-            n = len(nums)
-            arr = nums[:]  # make a copy
-            curr = start
-            direction = dir  # +1 (right) or -1 (left)
-            steps = 0  # ensure termination
-            while 0 <= curr < n and steps < n * 200:
-                if arr[curr] == 0:
-                    curr += direction
-                elif arr[curr] > 0:
-                    arr[curr] -= 1
-                    direction *= -1  # reverse direction
-                    curr += direction
-                steps += 1
-            return all(x == 0 for x in arr)
+    def countValidSelections(self, nums: List[int]) -> int:
 
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                if simulate(i, -1):  # try left
-                    count += 1
-                if simulate(i, 1):   # try right
-                    count += 1
-        return count
+        pref = list(accumulate(nums, initial = 0))
+        n, ans, sm = len(nums), 0, pref[-1]
+
+        for num, p in zip(nums, pref):
+            p *= 2
+
+            if num: continue
+            if sm == p: ans+= 2
+            if abs(sm - p) == 1: ans+= 1
+        
+        return ans

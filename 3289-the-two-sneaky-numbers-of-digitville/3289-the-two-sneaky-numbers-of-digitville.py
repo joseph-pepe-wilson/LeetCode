951894e5fe58d8236baf00e3d6c7e30d6c09807a
Joseph Wilson <145892873+joseph-pepe-wilson@users.noreply.github.com>
2025-10-31 23:32:33 +0530

    Time: 0 ms (100%), Space: 18.1 MB (20.87%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,7 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        return [num for num, freq in count.items() if freq == 2]
